68f88f82b7bb903003598623f4a3104087812acc
yienkung <yien.kung@gmail.com>
2017-06-11 16:56:53 +0800

    Calc 1

---
+++ Calculator1/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by Yi En Kung on 6/5/2017.
+//  Copyright © 2017 Yi En Kung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var result: UILabel!
+
+    @IBAction func ac(_ sender: UIButton) {
+    }
+    @IBAction func equals(_ sender: UIButton) {
+    }
+    @IBAction func zero(_ sender: UIButton) {
+    }
+    @IBAction func one(_ sender: UIButton) {
+    }
+    @IBAction func two(_ sender: UIButton) {
+    }
+    @IBAction func three(_ sender: UIButton) {
+    }
+    @IBAction func four(_ sender: UIButton) {
+    }
+    @IBAction func five(_ sender: UIButton) {
+    }
+    @IBAction func six(_ sender: UIButton) {
+    }
+    @IBAction func seven(_ sender: UIButton) {
+    }
+    @IBAction func eight(_ sender: UIButton) {
+    }
+    @IBAction func nine(_ sender: UIButton) {
+    }
+    @IBAction func plus(_ sender: UIButton) {
+    }
+    @IBAction func minus(_ sender: UIButton) {
+    }
+    @IBAction func multiply(_ sender: UIButton) {
+    }
+    @IBAction func divide(_ sender: UIButton) {
+    }
+    @IBAction func exponent(_ sender: UIButton) {
+    }
+    @IBAction func root(_ sender: UIButton) {
+    }
+    @IBAction func pi(_ sender: UIButton) {
+    }
+    @IBAction func ln(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
